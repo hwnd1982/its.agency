3ee10606ff0f8e9536ef60c5fbfbdaece775710d
Kirill Lavrov <lkv@myttk.ru>
2025-07-18 20:02:34 +0700

    fixed: api settings

--- src/shared/config/api.ts
+++ src/shared/config/api.ts
@@ -1,6 +1,3 @@
-export const YMAPS_API_KEY="9036b34f-cc88-4002-b747-0401d1eb3624";
-
-export const ROOT_API_URL = "http://45.153.69.44:3000"
-export const CART_API_URL="http://45.153.69.44:3000/cart";
-export const PRODUCTS_API_URL="http://45.153.69.44:3000/products";
-export const FILTERS_API_URL="http://45.153.69.44:3000/products/meta";
+export const ROOT_API_URL = "/api";
+export const CART_API_URL = "/api/cart";
+export const PRODUCTS_API_URL = "/api/products";
